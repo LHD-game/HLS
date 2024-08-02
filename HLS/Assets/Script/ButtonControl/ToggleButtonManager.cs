eef0406cf538dd35f079abdc8f4b65c7752ad295
나연 <109939867+eoyan@users.noreply.github.com>
2024-08-03 05:00:57 +0900

    버튼 오류 수정 완료, 여백 수정으로 문장 잘림 완화

--- HLS/Assets/Script/ButtonControl/ToggleButtonManager.cs
+++ HLS/Assets/Script/ButtonControl/ToggleButtonManager.cs
@@ -5,12 +5,9 @@ using UnityEngine.UI;
 public class ToggleButtonManager : MonoBehaviour
 {
     public List<ToggleButton> toggleButtons; // 모든 버튼 리스트
-    private List<bool> buttonStates; // 버튼 상태 리스트
 
     void Start()
     {
-        buttonStates = new List<bool>(new bool[toggleButtons.Count]);
-
         foreach (var toggleButton in toggleButtons)
         {
             toggleButton.button.onClick.AddListener(() => OnButtonClick(toggleButton));
@@ -19,23 +16,24 @@ public class ToggleButtonManager : MonoBehaviour
 
     void OnButtonClick(ToggleButton clickedButton)
     {
+        // 모든 버튼 상태를 초기화
+        foreach (var toggleButton in toggleButtons)
+        {
+            if (toggleButton != clickedButton)
+            {
+                toggleButton.ResetButtonState();
+            }
+        }
+
         // 클릭된 버튼의 상태를 토글
         clickedButton.ToggleButtonState();
-
-        // 클릭된 버튼의 상태를 업데이트
-        int index = toggleButtons.IndexOf(clickedButton);
-        if (index >= 0)
-        {
-            buttonStates[index] = clickedButton.IsSelected();
-        }
     }
 
     public void ResetButtonStates()
     {
-        for (int i = 0; i < toggleButtons.Count; i++)
+        foreach (var toggleButton in toggleButtons)
         {
-            toggleButtons[i].ResetButtonState();
-            buttonStates[i] = false;
+            toggleButton.ResetButtonState();
         }
     }
 }
