05cf321b5a9268c48fd591cd1db8ddc2a33669c5
나연 <109939867+eoyan@users.noreply.github.com>
2024-08-27 23:31:24 +0900

    피드백 반영 [처음부터] 버튼 [뒤로가기] 버튼 자식으로 배치, 불필요 오브젝트 삭제

--- HLS/Assets/Script/ButtonControl/RestartButtonHandler.cs
+++ HLS/Assets/Script/ButtonControl/RestartButtonHandler.cs
@@ -6,12 +6,9 @@ public class RestartButton : MonoBehaviour
     public Button restartButton; // Restart 버튼
     public ScoreManager scoreManager; // ScoreManager 스크립트
     public QuestionRenderer questionRenderer; // QuestionRenderer 스크립트
-    public Text restartButtonText;
 
     void Start()
     {
-        restartButtonText = restartButton.GetComponentInChildren<Text>();
-        restartButtonText.text = "다시";
         // Restart 버튼에 클릭 이벤트 추가
         restartButton.onClick.AddListener(OnRestartButtonClick);
     }
