b7f3570e501099909cb23df5eca52ad7b2603d77
sowon <sowon_c@naver.com>
2024-09-06 11:09:53 +0900

    0906

--- HLS/Assets/Script/WinCtl/WellcomeW.cs
+++ HLS/Assets/Script/WinCtl/WellcomeW.cs
@@ -7,23 +7,25 @@ public class WellcomeW : MonoBehaviour
 {
     public Animator Animator;
 
+    private float originWidth, originHeight;
+    public RectTransform parent;
+    public GridLayoutGroup Maingrid;
+    public GridLayoutGroup solugrid;
     private void Start()
     {
         originWidth = parent.rect.width;
         originHeight = parent.rect.height;
-        SetDynamicGrid(4, 2, 2);
+        SetDynamicGrid(Maingrid, 4, 2, 2);
+        SetDynamicGrid(solugrid, 9, 3, 3);
     }
     public void ClickWc()
     {
         Animator.SetTrigger("Login");
     }
 
-    private float originWidth, originHeight;
-    public RectTransform parent;
-    public GridLayoutGroup grid;
 
     // cnt : 컬럼 총 갯수, minColsInARow : 한 Row에 최소 컬럼 갯수, maxRow : 최대 Row 수.
-    public void SetDynamicGrid(int cnt, int minColsInARow, int maxRow)
+    public void SetDynamicGrid(GridLayoutGroup grid, int cnt, int minColsInARow, int maxRow)
     {
         int rows = Mathf.Clamp(Mathf.CeilToInt((float)cnt / minColsInARow), 1, maxRow + 1);
         int cols = Mathf.CeilToInt((float)cnt / rows);
